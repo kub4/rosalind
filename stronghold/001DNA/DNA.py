9d4b0ead3e33b47551ad3fc421a871d89ee94eaf
kub4 <jakub.friedl@gmail.com>
2014-10-25 03:16:49 +0200

    sample data/expected results in the comment

--- stronghold/001DNA/DNA.py
+++ stronghold/001DNA/DNA.py
@@ -14,6 +14,16 @@ Given: A DNA string s of length at most 1000 nt.
 Return: Four integers (separated by spaces) counting the respective number of
 times that the symbols 'A', 'C', 'G', and 'T' occur in s.
 
+Sample Dataset
+--------------
+
+AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC
+
+Sample Output
+-------------
+
+20 12 17 21
+
 """
 
 import sys
