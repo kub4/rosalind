6b2b969b18b84352968d808e5d9988e7e6bb373b
kub4 <jakub.friedl@gmail.com>
2014-10-27 14:36:10 +0100

    IPRB rewrite, TESTED, ACCEPTED

--- stronghold/04-IPRB/IPRB.py
+++ stronghold/04-IPRB/IPRB.py
@@ -28,9 +28,9 @@ import sys
 with open(sys.argv[1], 'r') as in_file:   # to automagically close the file
   data = list(map(int,in_file.readline().split()))
 
-k = data[0]   # dominant homozygotes
-m = data[1]   # heterozygotes
-n = data[2]   # recessive homozygotes
+k = data[0]   # dominant homozygotes (AA)
+m = data[1]   # heterozygotes (Aa)
+n = data[2]   # recessive homozygotes (aa)
 s = k+m+n     # sum of all individuals
 
 assert (k > 0), "k should be positive!"
@@ -38,20 +38,50 @@ assert (m > 0), "m should be positive!"
 assert (n > 0), "n should be positive!"
 assert (s > 1), "need a pair to have sex!"
 
-dom_child_probability = 0 # we start with zero
+'''
+a rewrite using a closed function, computing probability of recessive offspring
+(and then substracting  the result from one):
 
-# if "father" belongs to k, "mother" does not matter
-dom_child_probability += (k/s * 1)
+let's call the first parent "father", the second "mother" (but of course, we
+are dealing with hermafrodites)
 
-# if "father" belongs to n, "mother" decides
-dom_child_probability += (n/s * ((k/(s-1)) + (0.5*m)/(s-1)))
+1. if "father" is AA, there is no chance for a recessive child -> ignoring
 
-# if "father" belongs to m, both parents are important
-### (1) "father" belongs to m, "mother" to k:
-dom_child_probability += (m/s * k/(s-1))
-### (2) "father" belongs to m, "mother" to n:
-dom_child_probability += 0.5 * (m/s * n/(s-1))
-### (2) "father" belongs to m, "mother" also to m:
-dom_child_probability += 0.75 * (m/s * (m-1)/(s-1))
+2. if "father" is aa (n/s), all depends on mother:
+
+ 2a) "father" aa, "mother" AA -> no recessive children, ignoring
+
+ 2b) "father" aa, "mother" aa -> 100% of recessive children
+     (n/s)*((n-1)/(s-1))
+     -------------------
+ 
+ 2c) "father" aa, "mother" Aa -> 50% of recessive children
+     (n/s)*(m/(s-1))*0.5
+     -------------------
+
+3. if "father" is Aa (m/s), mother is important:
+
+ 3a) "father" Aa, "mother" AA -> no recessive children, ignoring
+
+ 3b) "father" Aa, "mother" aa -> 50% of recessive children
+     (m/s)*(n/(s-1))*0.5
+     -------------------
+ 
+ 3c) "father" Aa, "mother" Aa -> 25% of recessive children
+     (m/s)*((m-1)/(s-1))*0.25
+     ------------------------
+
+now we need to add all the underlined expressions
+
+(n/s)*((n-1)/(s-1)) + (n/s)*(m/(s-1))*0.5 +
++ (m/s)*(n/(s-1))*0.5 + (m/s)*((m-1)/(s-1))*0.25
+
+then simplify and substract from one...
+
+'''
+
+r = (n*(n-1) + n*m + 0.25*m*(m-1)) / (s*(s-1))
+
+dom_child_probability = 1 - r
 
 print(dom_child_probability)
