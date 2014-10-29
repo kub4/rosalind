7f793db62c681ad9bda668e048f36eaaac499b54
kub4 <jakub.friedl@gmail.com>
2014-10-29 10:13:31 +0100

    should now work, but we need to implement modulo!

--- stronghold/05-MRNA/MRNA.py
+++ stronghold/05-MRNA/MRNA.py
@@ -22,33 +22,36 @@ Sample Output
 
 """
 import sys
-from collections import Counter
 
-code = {
-"UUU" : "F", "CUU" : "L", "AUU" : "I", "GUU" : "V",
-"UUC" : "F", "CUC" : "L", "AUC" : "I", "GUC" : "V",
-"UUA" : "L", "CUA" : "L", "AUA" : "I", "GUA" : "V",
-"UUG" : "L", "CUG" : "L", "AUG" : "M", "GUG" : "V",
-"UCU" : "S", "CCU" : "P", "ACU" : "T", "GCU" : "A",
-"UCC" : "S", "CCC" : "P", "ACC" : "T", "GCC" : "A",
-"UCA" : "S", "CCA" : "P", "ACA" : "T", "GCA" : "A",
-"UCG" : "S", "CCG" : "P", "ACG" : "T", "GCG" : "A",
-"UAU" : "Y", "CAU" : "H", "AAU" : "N", "GAU" : "D",
-"UAC" : "Y", "CAC" : "H", "AAC" : "N", "GAC" : "D",
-"UAA" : "STOP", "CAA" : "Q", "AAA" : "K", "GAA" : "E",
-"UAG" : "STOP", "CAG" : "Q", "AAG" : "K", "GAG" : "E",
-"UGU" : "C", "CGU" : "R", "AGU" : "S", "GGU" : "G",
-"UGC" : "C", "CGC" : "R", "AGC" : "S", "GGC" : "G",
-"UGA" : "STOP", "CGA" : "R", "AGA" : "R", "GGA" : "G",
-"UGG" : "W", "CGG" : "R", "AGG" : "R", "GGG" : "G"
-}
+with open(sys.argv[1], 'r') as in_file:
+  protein = ''.join(in_file.read().split())
 
-values = []
+code_reversed = {
+"A" : 4,
+"C" : 2,
+"D" : 2,
+"E" : 2,
+"F" : 2,
+"G" : 4,
+"H" : 2,
+"I" : 3,
+"K" : 2,
+"L" : 6,
+"M" : 1,
+"N" : 2,
+"P" : 4,
+"Q" : 2,
+"R" : 6,
+"S" : 6,
+"T" : 4,
+"V" : 4,
+"W" : 1,
+"Y" : 2}
 
-for c in code:
-  values.append(code[c])
-c = Counter(values)
+# initialize the number of possible mRNAs with 3 possible STOP codons
+mrnas = 3
 
-for key, value in sorted(c.items()):
-  print('"'+str(key)+'"'+' : '+str(value))
-   
+for aa in protein:
+    mrnas *= code_reversed[aa]
+
+print(mrnas)
