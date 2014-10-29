622620d917dca17f21abe4651073be647a15df31
kub4 <jakub.friedl@gmail.com>
2014-10-29 15:09:38 +0100

    MRNA, modulo trick implemented, TESTED, ACCEPTED

--- stronghold/05-MRNA/MRNA.py
+++ stronghold/05-MRNA/MRNA.py
@@ -3,6 +3,18 @@
 """
 Inferring mRNA from Protein
 ===========================
+For positive integers a and n, a modulo n (written amodn in shorthand) is the
+remainder when a is divided by n. For example, 29mod11=7 because 29=11×2+7.  We
+say that a and b are congruent modulo n if amodn=bmodn; in this case, we use
+the notation a≡bmodn.
+
+Two useful facts in modular arithmetic are that if a≡bmodn and c≡dmodn, then
+a+c≡b+dmodn and a×c≡b×dmodn. To check your understanding of these rules, you
+may wish to verify these relationships for a=29, b=73, c=10, d=32, and n=11.
+
+As you will see in this exercise, some Rosalind problems will ask for a (very
+large) integer solution modulo a smaller number to avoid the computational
+pitfalls that arise with storing such large numbers.
 
 Given: A protein string of length at most 1000 aa.
 
@@ -51,9 +63,12 @@ code_reversed = {
 # initialize the number of possible mRNAs with 3 possible STOP codons
 mrnas = 3
 
+# python can deal with big numbers with no problems
+# modulo is needed only for the output, but implementing
+# the modulo trick anyway for algorhitmic/experimental reasons
+
 for aa in protein:
     mrnas *= code_reversed[aa]
+    mrnas %= 1000000
 
-# python can deal with big numbers with no problems
-# modulo needed only for the output
-print(mrnas % 1000000)
+print(mrnas)
