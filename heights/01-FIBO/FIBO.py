191dd7723fde67b5ba07c9aaf8c487ee1770cd56
kub4 <jakub.friedl@gmail.com>
2014-10-30 17:06:51 +0100

    FIBO, new combined version, TESTED, ACCEPTED

--- heights/01-FIBO/FIBO.py
+++ heights/01-FIBO/FIBO.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 """
-Fibonacci Numbers - naive exponential version
-=============================================
+Fibonacci Numbers
+=================
 
 The Fibonacci numbers 0,1,1,2,3,5,8,13,21,34,…
 
@@ -23,13 +23,26 @@ Sample Output
 """
 import sys
 
-# define Fibonacci function, the naive way
+# define Fibonacci function
 def fib(n):
+  if n==0:
+    return 0
+  f=[0, 1]
+  for i in range(2, n+1):
+    f.append(f[i-1]+f[i-2])
+  return f[n]
+
+"""
+#the above function is much faster (polynomial)
+#than the naive recursive approach (exponential): 
+"""
+# define Fibonacci function, the naive way
+def fib_naive(n):
   if n==0:
     return 0
   if n==1:
     return 1
-  return fib(n-1) + fib(n-2)
+  return fib_naive(n-1) + fib_naive(n-2)
 
 # use the command line argument as n (if int)
 # or use it to open a file, then sanity check
@@ -43,3 +56,4 @@ assert n>=0, "Need positive n!"
 
 # call Fibonacci function
 print(fib(n))
+#print(fib_naive(n))
