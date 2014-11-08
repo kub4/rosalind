45adfbdbf175bbf4923fdf7f270b71f534c0580c
kub4 <jakub.friedl@gmail.com>
2014-11-08 13:38:31 +0100

    CAT memoized, untested

--- stronghold/07-CAT/CAT.py
+++ stronghold/07-CAT/CAT.py
@@ -30,7 +30,9 @@ def count_noncrossing_perfect_matchings(rna):
   """
   Computes recursively the number of possible noncrossing perfect matchings
   in the RNA molecule. Assumes a valid RNA fulfilling the perfect matching
-  criteria (should be tested before calling this function).
+  criteria (should be tested before calling this function). Uses a global
+  dictionary called 'rnadict' for memoization. This dictionary must exist
+  and be empty before the primitive call to this function.
   """
   # if only two bases remain, and because we know the perfect matching
   # criteria are still honored, there is exactly one way to match
@@ -40,9 +42,13 @@ def count_noncrossing_perfect_matchings(rna):
   if l <= 2:
     return 1
 
+  # do not compute the same result more than once
+  if rna in rnadict:
+    return rnadict[rna]
+
   # create a list of all positions that, when bonded with the base at the
-  # first position (rna[0]), fulfill the noncrossing perfect matching criteria
-  # (i.e. new intervals contain the same number of 'A' as 'U' and 'C' as 'G') 
+  # first position (rna[0]), fulfill the perfect matching criteria (i.e.
+  # new intervals contain the same number of 'A' as 'U' and 'C' as 'G') 
   #
   # because we always start at the first position:
   # * we do not need to care about the circular shape of the graph
@@ -54,12 +60,17 @@ def count_noncrossing_perfect_matchings(rna):
   valid_bondings = [p for p in range(1,l,2) if rna[p]==pairing[rna[0]] and
                    test_interval(rna[1:p])]
 
-  # now count the perfect matchings
+  # now count the perfect matchings - we multiply numbers for two matching
+  # subintervals (as they can be freely combined) and sum numbers for
+  # all subinterval pairs (i.e. valid_bondings) 
   noncrossing_perfect_matchings = 0
   for p in valid_bondings:
     matches1 = count_noncrossing_perfect_matchings(rna[1:p])
     matches2 = count_noncrossing_perfect_matchings(rna[p+1:])
     noncrossing_perfect_matchings += matches1 * matches2
+
+  # memoize and return the number of noncrossing perfect matchings
+  rnadict[rna] = noncrossing_perfect_matchings
   return noncrossing_perfect_matchings
  
 def test_interval(rna):
@@ -85,6 +96,9 @@ if not test_interval(rna):
 
 # define base pairing rules
 pairing = {"A":"U", "U":"A", "C":"G", "G":"C"}
+
+# initialize a dictionary for memoization
+rnadict = {}
 
 # compute and print the result
 print(count_noncrossing_perfect_matchings(rna))
